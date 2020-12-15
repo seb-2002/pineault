783a61dfdc30c8edad8e7b01ccb3c73ff5b89ecb
Sebastian Kann <sebastiankann@gmail.com>
2020-12-15 15:43:35 -0500

    styling project page

--- single.php
+++ single.php
@@ -4,16 +4,23 @@
  while (have_posts()) {
    the_post();
 ?>
-<div class="heading__project">
-  <h2><?php the_title(); ?></h2>
-  <h2><?php the_excerpt(); ?></h2>
+
+<div class="container">
+  <div class="heading__project">
+    <h2><?php the_title(); ?></h2>
+    <h2><?php the_excerpt(); ?></h2>
+    
+    <a class="btn__home" href="<?php echo get_home_url(); ?>">Home</a>
   
-  <a href="<?php echo get_home_url(); ?>">Home</a>
+  </div>
+  
+  <img src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>">
+  
+  <div class="post-content">
+    <?php the_content(); ?>
+  </div>
 
 </div>
-
-<img src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>">
-<?php the_content(); ?>
 
 
 <?php }; ?>
